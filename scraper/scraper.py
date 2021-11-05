5fd300064a15930b25e9879d8638c48a3f90635f
ramapitekus <robert.oles@uzh.ch>
2021-11-05 20:14:10 +0100

    Add technical factors

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -2,49 +2,87 @@ from bs4 import BeautifulSoup
 import requests
 import pandas
 import re
+from datetime import datetime
 import os
-from typing import Tuple, List
+from typing import List, Tuple
 
 
-FACTORS = ['bitcoin-marketcap', 'bitcoin-price', 'transactions', 'bitcoin-sentinusd', 'bitcoin-transactionvalue',
+FACTORS = {"raw": ['bitcoin-marketcap', 'bitcoin-price', 'bitcoin-transactions', 'bitcoin-sentinusd', 'bitcoin-transactionvalue',
            'bitcoin-mediantransactionvalue', 'bitcoin-transactionfees', 'bitcoin-median_transaction_fee',
            'bitcoin-confirmationtime', 'bitcoin-size', 'bitcoin-fee_to_reward', 'bitcoin-difficulty',
-           'bitcoin-hashrate', 'bitcoin-mining_profitability', 'bitcoin-activeaddresses', 'bitcoin-tweets']
+           'bitcoin-hashrate', 'bitcoin-mining_profitability', 'bitcoin-activeaddresses', 'bitcoin-tweets', 'google_trends-btc',
+                   ],
+            "technical": ['price-btc', 'transactions-btc', 'size-btc', 'sentbyaddress-btc', 'difficulty-btc', 'hashrate-btc', 'mining_profitability-btc',\
+           'sentinusd-btc', 'transactionfees-btc', 'median_transaction_fee-btc', 'confirmationtime-btc', 'marketcap-btc',\
+           'transactionvalue-btc', 'mediantransactionvalue-btc', 'fee_to_reward-btc']}
 
-URL = "https://bitinfocharts.com/comparison/{}.html#alltime"
+PERIOD = ['3', '7', '14', '30', '90']
+INDICATORS = ['sma', 'ema', 'wma', 'std', 'var', 'trx', 'rsi', 'roc']
+START_DATE = datetime(2013, 4, 1)
+END_DATE = datetime(2020, 12, 31)
+URL = "https://bitinfocharts.com/comparison/{}{}.html#alltime"
 
 
-def main() -> None:
-    for factor in FACTORS:
-        date, val = scrape(url=URL.format(factor))
-        write_to_csv(date, val, factor)
+def collect() -> None:
+    vals = []
+    col_names = ["Date"]
+    col_names, vals, date = extract_data(FACTORS["raw"], col_names, vals, raw=True)
+    col_names, vals, date = extract_data(FACTORS["technical"], col_names, vals, raw=False)
+
+    df = pandas.DataFrame(list(zip(date, *vals)), columns=col_names)
+    write_to_csv(df)
 
 
-def scrape(url: str) -> Tuple:
-    flag = 0
+def extract_data(factors: List, col_names: List, vals: List, raw: bool) -> Tuple:
+    for factor in factors:
+        if not raw:
+            for indicator in INDICATORS:
+                for period in PERIOD:
+                    url = URL.format(factor, f'-{indicator}{period}')
+                    col_name = f'{factor}-{indicator}-{period}'
+                    val, col_name, date = scrape(url, col_name)
+                    vals.append(val)
+                    col_names.append(col_name)
+        else:
+            url = URL.format(factor, '')
+            col_name = f'{factor}_raw'
+            val, col_name, date = scrape(url, col_name)
+            vals.append(val)
+            col_names.append(col_name)
+    return col_names, vals, date  # noqa
+
+
+def scrape(url: str, col_name: str) -> Tuple:
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     scripts = soup.find_all('script')
     for script in scripts:
         if 'd = new Dygraph(document.getElementById("container")' in script.text:
-            flag = 1
             content = script.text
             content = '[[' + content.split('[[')[-1]
             content = content.split(']]')[0] + ']]'
             content = content.replace("new Date(", '').replace(')', '')
             data = parse_content(content)
 
-    if not flag:
-        raise AssertionError("Factor not found")
-
     date = []
     val = []
+    day = None
     for obs in data:  # noqa
         if (data.index(obs) % 2) == 0:
+            day = datetime.strptime(obs, "%Y/%m/%d")
+            if day < START_DATE:
+                continue
+            elif day > END_DATE:
+                break
             date.append(obs)
         else:
+            if day < START_DATE:
+                continue
+            elif day > END_DATE:
+                break
             val.append(obs)
-    return date, val
+
+    return val, col_name, date
 
 
 def parse_content(content: str) -> List:
@@ -54,12 +92,12 @@ def parse_content(content: str) -> List:
     return parsed
 
 
-def write_to_csv(date: List, val: List, factor: str) -> None:
-    df = pandas.DataFrame(list(zip(date, val)), columns=["Date", "val"])
+def write_to_csv(df) -> None:
     if not os.path.isdir('./scraper/data'):
         os.mkdir('./scraper/data')
 
-    df.to_csv(f'./scraper/data/{factor}.csv')  # noqa
+    df.to_csv(f'./scraper/data/indicators-{START_DATE.strftime("%d-%m-%Y")}-{END_DATE.strftime("%d-%m-%Y")}.csv')
 
 
-main()
+if __name__ == "__main__":
+    collect()
